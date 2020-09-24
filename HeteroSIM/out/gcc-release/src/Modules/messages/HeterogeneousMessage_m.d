38832a459021e79aebcbd40fd2ad539fdea0e333
MounaKaroui <mounaa.karouii@gmail.com>
2020-09-24 08:49:23 +0200

    updateMakefile

--- HeteroSIM/out/gcc-release/src/Modules/messages/HeterogeneousMessage_m.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-release/src/Modules/messages/HeterogeneousMessage_m.o: \
- Modules/messages/HeterogeneousMessage_m.cc \
- Modules/messages/HeterogeneousMessage_m.h \
- /home/mouna1/omnetpp-5.4.1-src-linux/omnetpp-5.4.1/include/omnetpp.h
-
-Modules/messages/HeterogeneousMessage_m.h:
-
-/home/mouna1/omnetpp-5.4.1-src-linux/omnetpp-5.4.1/include/omnetpp.h:
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Installation command:
 
 git clone --recurse-submodule https://github.com/MounaKaroui/HeteroSIM.git
 
+
+Required packages:
+
+libboost-dev ( for MCDM library)
