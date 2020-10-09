1bca9eb48e5fdc637b0300d03f4aae5b90a1ca30
MounaKaroui <mounaa.karouii@gmail.com>
2020-10-09 12:34:47 +0200

    Add control message

---
+++ HeteroSIM/src/Modules/application/ControlTrafficApp.cc
@@ -0,0 +1,54 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "ControlTrafficApp.h"
+
+Define_Module(ControlTrafficApp);
+
+void ControlTrafficApp::initialize()
+{
+
+    BaseAppl::initialize();
+    interfaceId=par("interfaceId").intValue();
+}
+
+void ControlTrafficApp::handleMessage(cMessage *msg)
+{
+
+    if(msg->isSelfMessage())
+         BaseAppl::handleMessage(msg);
+     else
+     {
+//         int arrivalGate=msg->getArrivalGateId();
+//         if(arrivalGate==fromDecisionMaker)
+//         {
+             // received from decider
+             // TODO add stats
+         }
+
+     }
+
+
+
+BasicMsg* ControlTrafficApp::BuildMsg(std::string namePrefix)
+{
+
+    ControlMsg*  ctrlMsg=new ControlMsg();
+    ctrlMsg->setName((namePrefix+string("-controlTraffic-")+std::to_string(ctrlMsg->getTreeId())).c_str());
+    ctrlMsg->setNetworkId(interfaceId);
+    ctrlMsg->setByteLength(msgLength);
+    return  ctrlMsg;
+}
+
